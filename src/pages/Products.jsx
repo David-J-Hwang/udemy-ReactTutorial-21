b6cb8a62575b49da426d0eda949d9f249a8f3f33
David J. Hwang <jeongjaehwang@gmail.com>
2024-03-19 16:48:11 +0900

    Start learning Section21-Routers

---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function ProductsPage() {
+  return (
+    <>
+      <h1>Products Page!</h1>
+
+      <p>Go to <Link to="/">Home</Link></p>
+    </>
+  )
+}
